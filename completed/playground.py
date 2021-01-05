03b8243eb8fb76d08ab976431ef47364ededf352
Gabby Chan <yccgabby@gmail.com>
2021-01-05 12:48:35 -0500

    Workshop reference files

--- completed/playground.py
+++ completed/playground.py
@@ -29,20 +29,19 @@ def every_film():
     return ", ".join([film["title"] for film in all_films["results"]])
 
 #TODO: 2. Create a method to allow a user to search a category by field
-def search_by_name(category, field):
+def search_by_category(category: str, field: str):
     return requests.get(base_url + category + "/?search=" + field).json()
 
 #TODO: 3. Given a character's name, return the planet that the character comes from, as well as the titles of the films the character appeared in
-def character_and_their_films_paragraph(name):
+def character_and_their_films_paragraph(name: str):
     character = requests.get(base_url + "people/?search=" + name).json()["results"][0]
     planet = requests.get(character["homeworld"]).json()["name"]
     films = [requests.get(url).json()["title"] for url in character["films"]]
     return(name + " is from " + planet + ". They have appeared in the following films: " + ", ".join(films) + ".")
 
-#TODO: 4. Print json in an organized way
 def pretty_print(response_json: json):
     print(json.dumps(response_json, indent=4, sort_keys=True))
 
 print(every_film())
-pretty_print(search_by_name("planets", "Tatooine"))
+pretty_print(search_by_category("planets", "Tatooine"))
 print(character_and_their_films_paragraph("Luke Skywalker"))
